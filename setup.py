2fb658b657cd2dd857f8c29070651268b268b49a
Thomas Hobson <git@hexf.me>
2021-04-11 13:24:57 +1200

    include gamma table

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="arls",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["arls=arls:main"],
     },
+    include_package_data=True,
+    package_data={
+        '': ["*.npy"]
+    }
 )
